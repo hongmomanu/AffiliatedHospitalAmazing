864fa51afdc9ea32077319d61309da78ef0668d4
jack <hongmomanu@163.com>
2015-05-05 15:57:25 +0800

    jack commit

--- app/view/outpatient/AppointmentDoctorList.js
+++ app/view/outpatient/AppointmentDoctorList.js
@@ -20,7 +20,52 @@ Ext.define('AffiliatedHospital.view.outpatient.AppointmentDoctorList', {
             }
         },
 
-        items: [],
+        items: [
+            {
+                xtype: 'toolbar',
+                height:25,
+                cls:'nopadding-toolbar',
+                docked: 'top',
+                items: [
+                    {
+                        xtype: 'selectfield',
+                        width:125,
+                        options: (function(){
+                            var data=[{text:'全部日期',value:'all'}];
+                            var now=new Date();
+                            for(var i=0;i<10;i++){
+                                var time=Ext.Date.add(now,Ext.Date.DAY,i);
+                                var timestr=Ext.Date.format(time,'Y-m-d');
+                                data.push({text:timestr,value:timestr});
+                            }
+                            return data;
+                        })()
+                    },
+                    {
+                        xtype: 'selectfield',
+                        width:65,
+                        options: [
+                            {text:'全天',value:'day'},
+                            {text:'上午',value:'am'},
+                            {text:'下午',value:'pm'}
+                        ]
+                    },
+                    {
+                        xtype: 'selectfield',
+                        width:95,
+                        options: [
+                            {text:'正副主任',value:'all'},
+                            {text:'正主任',value:'m'},
+                            {text:'副主任',value:'a'}
+                        ]
+                    }
+
+
+
+
+                ]
+            }
+        ],
         itemTpl: [
             '<table width="100%" height="100%"><tr>',
             '<td width="40%">',
