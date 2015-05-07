d618047c01fbf3c824475d0a3b1a5eb7c9e0f1f6
jack <hongmomanu@163.com>
2015-05-07 11:15:55 +0800

    jack commit

---
+++ app/model/healthwiki/HealthWiki.js
@@ -0,0 +1,9 @@
+Ext.define('AffiliatedHospital.model.healthwiki.PossibleDept', {
+    extend: 'Ext.data.Model',
+    config: {
+        fields: [
+            '_id',
+            'name'
+        ]
+    }
+});
---
+++ app/store/healthwiki/HealthWikis.js
@@ -0,0 +1,8 @@
+Ext.define('AffiliatedHospital.store.healthwiki.HealthWikis', {
+    extend: 'Ext.data.Store',
+    config: {
+        model: 'AffiliatedHospital.model.wisdomcare.HealthWiki',
+        autoLoad: true
+
+    }
+});
