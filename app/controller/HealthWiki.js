40b7bd9e72796a7c605435e17c18c84e29153438
jack <hongmomanu@163.com>
2015-05-07 11:18:36 +0800

    jack commit

---
+++ app/controller/HealthWiki.js
@@ -0,0 +1,61 @@
+/**
+ * Created by jack on 14-11-18.
+ * main Controller used by Terminal app
+ */
+Ext.define('AffiliatedHospital.controller.HealthWiki', {
+    extend: 'Ext.app.Controller',
+
+    config: {
+        views: [
+            'healthwiki.HealthWikiList'
+        ],
+        requires: [
+
+
+        ],
+        models: [
+            'healthwiki.HealthWiki'
+        ],
+        stores: [
+            'healthwiki.HealthWikis'
+
+        ],
+        control: {
+            nav: {
+                initialize: 'initRender'
+            },
+            healthwikilistview: {
+                itemtap: 'onHealthWikiItemSelect'
+            }
+
+        },
+        refs: {
+
+            nav: 'main',
+            healthwikilistview:'main #healthwikilist'
+
+
+
+        }
+    },
+
+
+    initRender: function () {
+
+
+    },
+    onHealthWikiItemSelect:function(list, index, node, record){
+        /*var nav=this.getNav();
+        var deptview=this.getPossibledeptlistview();
+        var store=deptview.getStore();
+        store.removeAll();
+        Ext.each(record.get('depts'),function(item){
+            store.add({name:item});
+        })*/
+
+
+    }
+
+
+
+});
